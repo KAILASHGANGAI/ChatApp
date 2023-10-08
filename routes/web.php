9ee04d5f0d5ad5e171140ee53910fdf84db6cb75
KAILASHGANGAI <47468706+KAILASHGANGAI@users.noreply.github.com>
2023-10-08 13:34:54 +0545

    websocket configration

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Events\WebSocketDemoEvent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    broadcast(new WebSocketDemoEvent('some-data'));
+    return view('welcome');
+});
